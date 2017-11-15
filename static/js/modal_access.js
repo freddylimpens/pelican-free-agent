e21e891c6507e45a7fda1858419a1f9174cd1c0f
cpicardlimpens <ccl.picard@gmail.com>
2017-11-15 16:42:42 +0100

    removing a image load in freeagent.css; back button when being on modal reload limpica.net; image-background for about

--- static/js/modal_access.js
+++ static/js/modal_access.js
@@ -8,6 +8,7 @@ Then stick this bit of Javascript at at the end of your document:
  */
 
  $(document).ready(function() {
+   var url = window.location.href;
    /* When accessing a specific work via a modal, the url is updated*/
    $(window.location.hash).modal('show');
    $('a[data-toggle="modal"]').click(function(){
@@ -16,10 +17,10 @@ Then stick this bit of Javascript at at the end of your document:
    function revertToOriginalURL() {
         var original = window.location.href.substr(0, window.location.href.indexOf('#'))
         history.replaceState({}, document.title, original);
+        history.pushState(null, null, original);
     }
 
     /* If using a specific url related to a work, we access the article directly*/
-    var url = window.location.href;
     for (var i = 0; i < 40; i++) {
         //console.log(i);
         if (url.indexOf('portfolioModal'+i) != -1) {
@@ -29,6 +30,17 @@ Then stick this bit of Javascript at at the end of your document:
 
     $('.modal').on('hidden.bs.modal', function () {
         revertToOriginalURL();
+    });
+
+    /* To manage the back button, and the closing of modal */
+    $(".modal").on("shown.bs.modal", function()  { // any time a modal is shown
+        var urlReplace = "#" + $(this).attr('id'); // make the hash the id of the modal shown
+        history.pushState(null, null, urlReplace); // push state that hash into the url
+    });
+
+    // If a pushstate has previously happened and the back button is clicked, hide any modals.
+    $(window).on('popstate', function() {
+        $(".modal").modal('hide');
     });
 
  });
